85c84b95ab08f19d0718b2ff0e0b81d2367b184f
Adam Basila <adamcabasila@gmail.com>
2018-04-16 13:47:36 -0600

    fixed syntax errors

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const server = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const data = require('./data/cohorts.json')
-const res = 'response'
-const req = 'request'
+
 
 function findById(data, id){
   for (var i = 0; i < data.length; i++) {
@@ -25,17 +24,17 @@ server.get('/:id', idRoute)
 
 function idRoute(req, res){
   var id = findById(data, req.params.id)
-  console.log(id)
   if (!id){
-    response.status(404).json({
+    res.status(404).json({
       error:{
         message:"No record found"
       }
-  })
-  else {
+    })
+  }
+  else{
     res.json({data: id})
     }
-  }
 }
+
 
 server.listen(port)
